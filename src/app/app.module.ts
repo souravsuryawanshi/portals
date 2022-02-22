49fc6e2dfc3c43e977ebd2968081392c8aedd905
Sourav Suryawanshi <23135356@AFRICA.AIRTEL.ITM>
2022-02-22 09:57:28 +0530

    barelling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './ui/navbar/navbar.component';
-import { SearchbarComponent } from './ui/navbar/searchbar/searchbar.component';
-import { LinksComponent } from './ui/navbar/links/links.component';
-import { FormComponent } from './ui/form/form.component';
-import { InputComponent } from './ui/form/input/input.component';
-import { LoginComponent } from './ui/form/login/login.component';
-import { LogoComponent } from './ui/logo/logo.component';
-import { MockComponent } from './ui/mock/mock.component';
-import { BurgerComponent } from './ui/navbar/burger/burger.component';
-import { TextLinkComponent } from './ui/textlink/textlink.component';
+import { Components, bootstrap } from './declarations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent,
-    NavbarComponent,
-    LinksComponent,
-    SearchbarComponent,
-    FormComponent,
-    InputComponent,
-    LoginComponent,
-    LogoComponent,
-    MockComponent,
-    BurgerComponent,
-    TextLinkComponent,
-  ],
+  declarations: [Components],
   imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [bootstrap],
 })
 export class AppModule {}
---
+++ src/app/declarations.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {
+  FormComponent,
+  InputComponent,
+  LoginComponent,
+  LogoComponent,
+  MockComponent,
+  NavbarComponent,
+  BurgerComponent,
+  LinksComponent,
+  SearchbarComponent,
+  TextLinkComponent,
+} from './ui';
+
+export const Components = [
+  AppComponent,
+  FormComponent,
+  NavbarComponent,
+  LinksComponent,
+  SearchbarComponent,
+  FormComponent,
+  InputComponent,
+  LoginComponent,
+  LogoComponent,
+  MockComponent,
+  BurgerComponent,
+  TextLinkComponent,
+];
+
+export const bootstrap = [AppComponent];
